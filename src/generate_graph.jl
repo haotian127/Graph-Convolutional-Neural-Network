0d03d9bad1abb0a95c21871e61df4e07abc67b63
Haotian <htsli@ucdavis.edu>
2019-12-08 22:51:48 -0800

    consider delaunay triangulation

--- src/generate_graph.jl
+++ src/generate_graph.jl
@@ -1,3 +1,38 @@
-using Plots, LightGraphs
+using Plots, LightGraphs, CSV, LinearAlgebra, VoronoiDelaunay
 
-include(joinpath("..", "src", "gplot.jl"))
+include("F://GitHub//Graph-Convolutional-Neural-Network//src//gplot.jl")
+include("F://GitHub//Graph-Convolutional-Neural-Network//src//helpers.jl")
+
+df = CSV.read("F://GitHub//Graph-Convolutional-Neural-Network//dataset//singapore_airbnb.csv")
+
+latitude = df.latitude[:]
+longitude = df.longitude[:]
+price = df.price[:]; price[price .> 250] .= 250
+
+X = hcat(longitude, latitude)
+
+scatter_gplot(X; marker = price, ms = 3)
+
+N = size(X,1)
+dist = zeros(N,N); for i = 1:N-1, j = i+1:N; dist[i,j] = norm(X[i,:] - X[j,:]); end; dist = dist + dist';
+
+tess = DelaunayTessellation()
+a = Point2D[Point(X[i,1], X[i,2]) for i in 1:N]
+push!(tess, a)
+
+G = Graph(N)
+for edge in delaunayedges(tess)
+    add_edge!(G,Edge(geta(edge),getb(edge)))
+end
+
+# ## build k-nearest neighbor graph, k = 3,5
+# G = Graph(N)
+# for v = 1:N
+#     dist2v = dist[v,:]
+#     for nb in findminimum(dist[v,:], 3)
+#         add_edge!(G,Edge(v,nb))
+#     end
+# end
+#
+# gplot(1.0 .* adjacency_matrix(G), X; width = 1); plt = plot!(aspect_ratio = 1)
+# # savefig(plt, "figs\\3-NN-graph.png")
